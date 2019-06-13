a1390ab738ec39f92b570cf31ef1df90c0b11631
leog91 <leog91@gmail.com>
2019-06-13 05:01:42 -0300

    Updates

---
+++ precache-manifest.ff19214adc9f6240323a084059e4b9b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7e6e5b4d219604463b4c350da3fb4a0",
+    "url": "/playground/index.html"
+  },
+  {
+    "revision": "180778101ffde69edbcc",
+    "url": "/playground/static/css/main.573af274.chunk.css"
+  },
+  {
+    "revision": "d278e8ff3a18ef1e3e05",
+    "url": "/playground/static/js/2.e29d3923.chunk.js"
+  },
+  {
+    "revision": "180778101ffde69edbcc",
+    "url": "/playground/static/js/main.7f1761dd.chunk.js"
+  },
+  {
+    "revision": "49effa767ea8d7bd1610",
+    "url": "/playground/static/js/runtime~main.c80ac52c.js"
+  },
+  {
+    "revision": "e9f83e7ec2c9c88cf950171ebf09f056",
+    "url": "/playground/static/media/snow-dog.e9f83e7e.jpg"
+  }
+]);
